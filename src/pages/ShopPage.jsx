4026c83810583d0e9d23f134d4938e99800aab54
aaryandevwork <aaryandevwork@gmail.com>
2026-08-04 15:46:14 +0530

    feat : all filter feature implimented

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,9 +1,55 @@
+import { useMemo, useState } from "react";
 import ProductCard from "../components/ProductCard";
-import { products } from "../data/data";
+import { categories, products, sortOptions } from "../data/data";
 import { Search } from "lucide-react";
+import { useEffect } from "react";
 
 const ShopPage = () => {
-  console.log(products);
+
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [sortBy, setSortBy] = useState("Featured");
+  const [search, setSearch] = useState("");
+
+  
+  const getFilteredData = () => {
+    let filteredData = [...products];
+
+    if (search.trim()) {
+      filteredData = filteredData.filter((product) => {
+        return product.title.toLowerCase().includes(search.toLowerCase());
+      });
+    }
+
+    if (selectedCategory !== "All") {
+      filteredData = filteredData.filter((product) => {
+        return product.category === selectedCategory;
+      });
+    }
+
+    switch (sortBy) {
+      case "Price: Low to High":
+        filteredData.sort((a, b) => a.price - b.price);
+        break;
+      case "Price: High to Low":
+        filteredData.sort((a, b) => b.price - a.price);
+        break;
+      case "Highest Rated":
+        filteredData.sort((a, b) => b.rating - a.rating);
+        break;
+      case "Newest":
+        filteredData.sort((a, b) => b.id - a.id);
+        break;
+      default:
+        filteredData.sort((a, b) => b.featured - a.featured);
+        break;
+    }
+    console.log("called");
+    return filteredData;
+  };
+
+  const finalProducts = useMemo(() => {
+    return getFilteredData();
+  }, [products, search, selectedCategory, sortBy]);
 
   return (
     <div className="px-35 py-5 flex flex-col ">
@@ -23,6 +69,7 @@ const ShopPage = () => {
             <Search className="absolute left-5 top-1/2 -translate-y-1/2 text-zinc-500" />
 
             <input
+              onChange={(e) => setSearch(e.target.value)}
               placeholder="Search products..."
               className="
                 w-full
@@ -34,10 +81,10 @@ const ShopPage = () => {
                 pr-2
                 py-2
                 transition-all
-    duration-300
-    focus:border-lime-400
-    focus:ring-4
-    focus:ring-lime-400/20
+                duration-300
+              focus:border-lime-400
+                focus:ring-4
+              focus:ring-lime-400/20
                 outline-none
                 "
             />
@@ -46,35 +93,37 @@ const ShopPage = () => {
           {/* Category */}
 
           <select
+            onChange={(e) => setSelectedCategory(e.target.value)}
             className="lg:col-span-2 bg-[#1d1d1d] rounded-2xl px-5 text-white transition-all
     duration-300
     focus:border-lime-400
     focus:ring-4
     focus:ring-lime-400/20 "
           >
-            <option>All</option>
-            <option>Electronics</option>
-            <option>Fashion</option>
+            {categories.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
           </select>
 
           {/* Sort */}
 
           <select
-            className="lg:col-span-2 bg-[#1d1d1d] rounded-2xl px-5 text-white transition-all
-    duration-300
-    focus:border-lime-400
-    focus:ring-4
-    focus:ring-lime-400/20"
+            onChange={(e) => setSortBy(e.target.value)}
+            className="lg:col-span-2 bg-[#1d1d1d] rounded-2xl px-5 text-white transition-all duration-300 focus:border-lime-400 focus:ring-4 focus:ring-lime-400/20"
           >
-            <option>Featured</option>
-            <option>Price Low</option>
-            <option>Price High</option>
+            {sortOptions.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
           </select>
         </div>
       </div>
 
       <div className="grid mt-12 gap-7 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5">
-        {products.map((product) => (
+        {finalProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
