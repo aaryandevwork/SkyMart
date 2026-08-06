90412774e01a267075d1eda90a10f71e8e84889a
aaryandevwork <aaryandevwork@gmail.com>
2026-08-06 23:38:21 +0530

    fix : totalbill pointer number problem solved using toFixed(2)

--- src/components/CartSideBar.jsx
+++ src/components/CartSideBar.jsx
@@ -13,7 +13,7 @@ const CartSideBar = () => {
     return cartItems.reduce(
       (total, item) => total + item.price * item.quantity,
       0,
-    );
+    ).toFixed(2);
   }, [cartItems]);
 
   useEffect(() => {
